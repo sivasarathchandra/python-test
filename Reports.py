c8551b4c12a41a806449dbe0f2eb4881d91aee18
Sarath Chandra <chsivasarath@gmail.com>
2017-06-04 22:11:03 -0700

    Add files via upload

--- Reports.py
+++ Reports.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 import requests
 import re
+import sys
 
-USERNAME = "888888"
-PASSWORD = "8888888"
+
+USERNAME = "shiva@near.co" 
+PASSWORD = "hitman1251"
 LOGIN_URL = "https://engage.adnear.net/login"
 GET_URL = "https://engage.adnear.net/AllSpark-Main/campaigns/589acc267e4fb73c4500043c/api/reports/4?report_start_date=2017-02-15&report_end_date=2017-05-10&aggregation=date"
 
+sys.stdout = open("junk1","w").encode('utf8')
 session = requests.Session()
 response = session.get(LOGIN_URL)
 
@@ -20,8 +23,8 @@ print token
 data = {
 	"utf8" : True,
 	"authenticity_token" : token,
-	"user[email]" : "8888888",
-	"user[password]" : "8888888",
+	"user[email]" : "shiva@near.co",
+	"user[password]" : "hitman1251",
 	"user[remember_me]" : "0",
 	"commit" : ""
 }
